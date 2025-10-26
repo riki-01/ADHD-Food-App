685af25e901787fc5233125dcc2dc3f927ccf429
riki-01 <karanjitrikita47@gmail.com>
2025-10-27 01:14:02 +0600

    added static data

---
+++ ADHD_mobile/services/dump.js
@@ -0,0 +1,68 @@
+
+//ids here are 6digit random strings. this is just for dummy data
+var data = {
+    "users": {
+      "du988w": {
+
+      }
+    },
+    "application-options": {
+      "dietaryGoals": [
+        "weight_loss",
+        "muscle_gain",
+        "clean_eating",
+        "keto",
+        "vegan"
+      ],
+      "medicalConditions": [
+        "ADHD",
+        "Anxiety",
+        "Diabetes",
+        "Hypertension"
+      ],
+      "bloodGroups": [
+        "A+",
+        "A-",
+        "B+",
+        "B-",
+        "AB+",
+        "AB-",
+        "O+",
+        "O-"
+      ],
+      "profileOptions": [
+        {
+          "id": "Ax7tR9",
+          "title": "Personal Information",
+          "icon": "person-outline"
+        },
+        {
+          "id": "yP3X9z",
+          "title": "Dietary Preferences",
+          "icon": "restaurant-outline"
+        },
+        {
+          "id": "n4LqT2",
+          "title": "Medical Conditions",
+          "icon": "medical-outline"
+        },
+        {
+          "id": "Us8Rm1",
+          "title": "Notifications",
+          "icon": "notifications-outline"
+        },
+        {
+          "id": "qW5hZ0",
+          "title": "Privacy & Security",
+          "icon": "shield-outline"
+        },
+        {
+          "id": "Jk2bM6",
+          "title": "Help & Support",
+          "icon": "help-circle-outline"
+        }
+      ]
+    }
+  };
+
+export default data;
