2f1dbd027e604be57f39e6ea3c15708cdefa50ee
Saina555 <sainashrestha08@gmail.com>
2025-11-22 16:36:04 +0600

    added layout file

---
+++ ADHD_mobile/app/_layout.tsx
@@ -0,0 +1,23 @@
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export const unstable_settings = {
+  anchor: 'login',
+};
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="chat" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="light" />
+    </ThemeProvider>
+  );
+}
